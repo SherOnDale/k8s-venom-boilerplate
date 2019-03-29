1df39cd332a0aa44bb68a931f15a573322895656
Sherin Binu <sherinbinu@hotmail.com>
2019-03-29 16:37:57 +0530

    generic deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t sherondale/multi-docker-client:latest -t sherondale/multi-docker-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sherondale/multi-docker-server:latest -t sherondale/multi-docker-server:$SHA -f ./server/Dockerfile ./server
-docker push sherondale/multi-docker-client:latest
-docker push sherondale/multi-docker-server:latest
+docker build -t <CLIENT_IMAGE_NAME>:latest -t <CLIENT_IMAGE_NAME>:$SHA -f ./client/Dockerfile ./client
+docker build -t <SERVER_IMAGE_NAME>:latest -t <SERVER_IMAGE_NAME>:$SHA -f ./server/Dockerfile ./server
+docker push <CLIENT_IMAGE_NAME>:latest
+docker push <SERVER_IMAGE_NAME>:latest
 
-docker push sherondale/multi-docker-client:$SHA
-docker push sherondale/multi-docker-server:$SHA
+docker push <CLIENT_IMAGE_NAME>:$SHA
+docker push <SERVER_IMAGE_NAME>:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/multi-docker-server-deployment multi-docker-server=sherondale/multi-docker-server:$SHA
-kubectl set image deployments/multi-docker-client-deployment multi-docker-client=sherondale/multi-docker-client:$SHA
+kubectl set image deployments/multi-docker-server-deployment multi-docker-server=<SERVER_IMAGE_NAME>:$SHA
+kubectl set image deployments/multi-docker-client-deployment multi-docker-client=<CLIENT_IMAGE_NAME>:$SHA
